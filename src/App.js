06082b671cac696633bc44797bc3b6341aa7f7e4
AirtonDoug <airtonservodecristo@outlook.com>
2023-03-30 13:32:09 -0300

    commit no vscode

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 //import HelloWorld from './components/00HelloWorld';
 //import Calculadora from './components/01Calculadora';
-import './App.css';
-import MyProps from './components/02MyProps';
-function App() {
+//import MyProps from './components/02MyProps';
+//import Pai from './components/atividade01/questao01/01Pai'
+import * as PC from './components/atividade01/questao02/02MeuPC'
+/*function App() {
   return (
     <MyProps
       nome="Airton Douglas Costa de Sousa"
@@ -11,5 +12,19 @@ function App() {
     />
   );
 }
+*/
+
+function App(){
+  return(
+      <div>
+        <PC.Memoria nome = "HyperX" preco={350}/>
+        <PC.PlacaDeVideo nome =  "GeForce" preco = {3980}/>
+        <PC.PlacaMae nome = "ASUS" preco={1200}/>
+      </div>
+    
+  )
+}
+
+
 
 export default App;
---
+++ src/components/atividade01/questao02/02MeuPC.jsx
@@ -0,0 +1,9 @@
+export const PlacaDeVideo = ({ nome, preco }) => {
+    <h3>Placa de Video: {nome} preco: {preco}</h3>
+}
+export const PlacaMae = ({ nome, preco }) => {
+    <h3>Placa de Video: {nome} preco: {preco}</h3>
+}
+export const Memoria = ({ nome, preco }) => {
+    <h3>Placa de Video: {nome} preco: {preco}</h3>
+}
