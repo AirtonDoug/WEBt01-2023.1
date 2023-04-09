f352b4ec01a27e8f1a37ed34e7cfda3c728aecd0
AirtonDoug <airtonservodecristo@outlook.com>
2023-04-09 06:49:56 -0300

    organizando pastas Storage

--- src/components/atividade01/questao02/02MeuPC.jsx
+++ src/components/atividade01/questao02/02MeuPC.jsx
@@ -1,9 +1,10 @@
 export const PlacaDeVideo = ({ nome, preco }) => {
-    <h3>Placa de Video: {nome} preco: {preco}</h3>
+    return <h3>Placa de Video: {nome} preco: {preco}</h3>
+    
 }
 export const PlacaMae = ({ nome, preco }) => {
-    <h3>Placa de Video: {nome} preco: {preco}</h3>
+    return <h3>Placa de Video: {nome} preco: {preco}</h3>
 }
 export const Memoria = ({ nome, preco }) => {
-    <h3>Placa de Video: {nome} preco: {preco}</h3>
+    return <h3>Placa de Video: {nome} preco: {preco}</h3>
 }
